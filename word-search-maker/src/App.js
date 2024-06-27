5d107711aaddb61424f5d96d44bf457f7e9adf28
hhaemin <ahrgoals@naver.com>
2024-06-27 16:39:13 +0900

    배경색 추가

--- word-search-maker/src/App.js
+++ word-search-maker/src/App.js
@@ -2,8 +2,12 @@ import "./App.css";
 import Header from "./component/Header";
 
 function App() {
+  const appStyle = {
+    paddingLeft: "35px",
+    paddingRight: "35px",
+  };
   return (
-    <div>
+    <div className="wrapper" style={appStyle}>
       <Header />
     </div>
   );
