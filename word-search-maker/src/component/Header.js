86fe3822991c235edc3a7c67d552de810bed4bc9
hhaemin <ahrgoals@naver.com>
2024-07-03 00:17:58 +0900

    메뉴바 수정중

--- word-search-maker/src/component/Header.js
+++ word-search-maker/src/component/Header.js
@@ -11,7 +11,15 @@ const Header = () => {
           <div className="subTitle-3">Search</div>
         </div>
       </div>
-      <button className="menu-btn">Menu</button>
+      {/* <button className="menu-btn">Menu</button> */}
+      <div className="menu-bar">
+        <div className="changeTheme">Switch Theme</div>
+        <div className="icon">
+          <div className="icon-1">Word Search Maker</div>
+          <div className="icon-2">More Puzzles</div>
+          <div className="icon-3">Search</div>
+        </div>
+      </div>
     </header>
   );
 };
